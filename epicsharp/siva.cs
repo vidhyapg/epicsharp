e12c29cc3b032bfd2e94a7d6d81bf8a53caad68b
Vidhya Palaniswamy Gnanam <vidpal@microsoft.com>
2017-11-26 03:28:53 -0800

    created proj structure

---
+++ epicsharp/siva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epicsharp
+{
+    class Siva
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello Siva");
+        }
+    }
+}
